a1f3211003695090d39c48fe2b8648ff887d462b
Anannapath <152575328+Anannapath@users.noreply.github.com>
2025-01-08 15:06:03 +0700

    Passing Argumets

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Header,Content,Footer  } from './func-components';
 import Button from './class-components';
+import {Calculator2} from './calculator';
 
 
 function App() {
 
-  return <Button/>
+  return <Calculator2/>
 
 }
 
