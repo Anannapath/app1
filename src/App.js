53f2f7e90d88d8343e5b54b1ed80ece3b5dce37d
Anannapath <152575328+Anannapath@users.noreply.github.com>
2025-01-08 15:30:38 +0700

    Add EventData

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Header,Content,Footer  } from './func-components';
 import Button from './class-components';
 import {Calculator2} from './calculator';
+import { EventData2 } from './event-data';
+
 
 
 function App() {
 
-  return <Calculator2/>
+  return <EventData2/>
 
 }
 
