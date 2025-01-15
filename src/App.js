c3ff79625a58f87d2a6dda53d23252a521b9a81f
Anannapath <152575328+Anannapath@users.noreply.github.com>
2025-01-15 14:09:41 +0700

    References Function Components
     refs-func.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { Header,Content,Footer  } from './func-components';
-import Button from './class-components';
-import {Calculator2} from './calculator';
-import { EventData2 } from './event-data';
-
-
+import RefsFunc from './refs-func';
 
 function App() {
 
-  return <EventData2/>
+  return <RefsFunc/>
 
 }
 
